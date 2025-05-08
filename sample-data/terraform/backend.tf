7fc3ffaaa72bc556cdd3931c2c6668107674449f
gwmack@google.com <gwmack@google.com>
2025-05-08 01:55:39 +0000

    team variables setup

--- sample-data/terraform/backend.tf
+++ sample-data/terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "gcs" {
-    bucket = "<TERRAFORM_BUCKET>"
-    prefix = "<TERRAFORM_ENV>"
+    bucket = "aef-aef-east-coast-remote-tfe"
+    prefix = "sample-data/environments/dev"
   }
 }
